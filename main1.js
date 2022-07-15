d522974cbbc6a996a27b48e9d87a24df84c6fb54
Mohammad Alamin <86641758+alamin500@users.noreply.github.com>
2022-07-15 15:55:32 +0600

    f

---
+++ main1.js
@@ -0,0 +1,11 @@
+const luckyNoBtn = document.querySelector("#luckyNoBtn");
+const luckyNoDisplay = document.querySelector("#winningNoDis");
+let luckyNumber = 0;
+// luckyNoBtn.addEventListener("click", () => {
+//   // luckyNumber = Math.floor(Math.random() * 10) + 1;
+//   // luckyNoDisplay.textContent = luckyNumber;
+//   console.log("Hi");
+// });
+luckyNoBtn.addEventListener("click", () => {
+  console.log("hi");
+});
